6e7a97aac811cd7d9cee20e3a1059342b20fe3f2
Cody Keats <95243190+cocopuff2u@users.noreply.github.com>
2023-09-29 20:10:19 +0300

    Update IP_GEO_Location.sh

--- Extension Attributes/IP_GEO_Location.sh
+++ Extension Attributes/IP_GEO_Location.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-# Geo-IP Location
 # Gets an approximation of the computer's current location based on IP
 
 myIP=$(curl -L -s --max-time 10 http://checkip.dyndns.org | egrep -o -m 1 '([[:digit:]]{1,3}\.){3}[[:digit:]]{1,3}')
