2ceaaf5ab7cd60dea712d263188af75e2861f99d
Cody Keats <95243190+cocopuff2u@users.noreply.github.com>
2024-03-06 11:38:19 -0500

    Update TrellixLoopUpdates.sh

--- Other_Scripts/TrellixLoopUpdates.sh
+++ Other_Scripts/TrellixLoopUpdates.sh
@@ -4,7 +4,7 @@
 #
 # # Trellix Loop Update Selection
 #
-# Purpose: Loop Trellix Updates 5 or 20 times with swiftdialog prompt
+# Purpose: Prompts users to select loop of Trellix Updates
 # 
 # https://github.com/cocopuff2u
 #
@@ -12,10 +12,69 @@
 #
 #   History
 #
-#  1.0 03/01/24 Original
+#  1.0 03/01/24 - Original
 #
+#  1.1 03/06/24 - Gave the user a selective prompt for various loops, fixed logging, and icon check
 #
 ####################################################################################################
+
+# Path to our dialog binary
+dialogPath='/usr/local/bin/dialog'
+# Path to our dialog command file
+dialogCommandFile=$(mktemp /var/tmp/trellixloopDialog.XXXXX)
+#Note: This can be BASE64, a Local File or a URL
+icon="/Applications/Trellix Endpoint Security for Mac.app/Contents/Resources/McAfee_Locked.png"
+#"Window Title"
+titleloop="Trellix Update In Progress"
+title="Trellix Loop Utility"
+#"Window Message During Loop"
+descriptionloop="May take a few minutes"
+#"Window Message To Select Loop"
+description="Choose the number of loops for executing Trellix updates"
+#Help Message
+helpmessage="This script is crafted to automate essential Trellix update functions on MacOS in conjunction with the EPO server. The loop iterates through all permissible commands to EPO, incorporating a sleep function between each command for optimal execution."
+# Script Log Location [ /var/log/org.YOURCOMPANY.log ] (i.e., Your organization's default location for client-side logs)
+scriptLog="${4:-"/var/log/org.trellixloopupdate.log"}"                       
+
+#"Button1"
+button1="Exit"
+#"Button2"
+button2="Select"
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Pre-flight Check: Client-side Logging
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+if [[ ! -f "${scriptLog}" ]]; then
+    touch "${scriptLog}"
+fi
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Pre-flight Check: Client-side Script Logging Function
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+function updateScriptLog() {
+    echo -e "$( date +%Y-%m-%d\ %H:%M:%S ) - ${1}" | tee -a "${scriptLog}"
+}
+
+updateScriptLog "Trellix Loop Updates: Starting...."
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Pre-flight Check: Checking for Icon Availability 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+updateScriptLog "Trellix Loop Updates: Checking for icon path...."
+
+if [ ! -e "$icon" ]; then
+    # The path is missing, so perform action x
+    updateScriptLog "Trellix Loop Updates: icon path not valid, using default icon...."
+    icon="SF=clock.arrow.2.circlepath,colour=green,colour2=blue,weight=bold"
+else
+    updateScriptLog "Trellix Loop Updates: Icon valid, proceeding...."
+fi
+
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
@@ -30,7 +89,7 @@ function dialogInstall() {
     # Expected Team ID of the downloaded PKG
     expectedDialogTeamID="PWA5E9TQ59"
 
-    updateScriptLog "SELF HEAL: Installing swiftDialog..."
+    updateScriptLog "Trellix Loop Updates: Installing swiftDialog...."
 
     # Create temporary working directory
     workDirectory=$( /usr/bin/basename "$0" )
@@ -48,7 +107,7 @@ function dialogInstall() {
         /usr/sbin/installer -pkg "$tempDirectory/Dialog.pkg" -target /
         sleep 2
         dialogVersion=$( /usr/local/bin/dialog --version )
-        updateScriptLog "SELF HEAL: swiftDialog version ${dialogVersion} installed; proceeding..."
+        updateScriptLog "Trellix Loop Updates: swiftDialog version ${dialogVersion} installed; proceeding...."
 
     else
 
@@ -67,7 +126,7 @@ function dialogCheck() {
     # Check for Dialog and install if not found
     if [ ! -e "/Library/Application Support/Dialog/Dialog.app" ]; then
 
-        updateScriptLog "SELF HEAL: swiftDialog not found. Installing..."
+        updateScriptLog "Trellix Loop Updates: swiftDialog not found. Installing...."
         dialogInstall
 
     else
@@ -75,12 +134,12 @@ function dialogCheck() {
         dialogVersion=$(/usr/local/bin/dialog --version)
         if [[ "${dialogVersion}" < "${swiftDialogMinimumRequiredVersion}" ]]; then
             
-            updateScriptLog "SELF HEAL: swiftDialog version ${dialogVersion} found but swiftDialog ${swiftDialogMinimumRequiredVersion} or newer is required; updating..."
+            updateScriptLog "Trellix Loop Updates: swiftDialog version ${dialogVersion} found but swiftDialog ${swiftDialogMinimumRequiredVersion} or newer is required; updating...."
             dialogInstall
             
         else
 
-        updateScriptLog "SELF HEAL: swiftDialog version ${dialogVersion} found; proceeding..."
+        updateScriptLog "Trellix Loop Updates: swiftDialog version ${dialogVersion} found; proceeding...."
 
         fi
     
@@ -91,54 +150,53 @@ function dialogCheck() {
 dialogCheck
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# Flight: Prompts users to select 5 Loop or 20 loop of Trellix Updates
+# Flight: Prompts users to select loop of Trellix Updates
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-# Path to our dialog binary
-dialogPath='/usr/local/bin/dialog'
-# Path to our dialog command file
-dialogCommandFile=$(mktemp /var/tmp/exampleDialog.XXXXX)
-#Note: This can be BASE64, a Local File or a URL
-icon="/Applications/Trellix Endpoint Security for Mac.app/Contents/Resources/McAfee_Locked.png"
-#"Window Title"
-titleloop="Trellix Update In Progress"
-title="Trellix Loop Utility"
-#"Window Message"
-descriptionloop="May take a few minutes"
-description="Choose the number of loops for executing Trellix updates"
+# This function sends a command to our command file, and sleeps briefly to avoid race conditions
+function dialog_command()
+{
+echo "$@" >> "$dialogCommandFile"
+sleep 0.1
+}
 
-#Help Message
-helpmessage="If you need to kill this script 
+# Needed a slight delay with DialogCheck
+sleep 0.1
 
-Sudo killall Dialog
-"
+updateScriptLog "Trellix Loop Updates: Prompting user for loop selection...."
 
-#"Button1"
-button1="5 Loops"
-#"Button2"
-button2="20 Loops"
+exit_status=$(
+    $dialogPath \
+    --title "$title" \
+    --message "$description" \
+    --ontop \
+    --commandfile "$dialogCommandFile" \
+    --icon "$icon" \
+    --button1 \
+    --button1text "$button2" \
+    --button2 \
+    --button2text "$button1" \
+    --selecttitle "Select below the amount of loop cycles:",radio \
+    --selectvalues "5 Loops, 10 Loops, 20 Loops, 50 Loops" \
+    --helpmessage "$helpmessage" \ | grep "SelectedOption" | awk -F ": " '{print $NF}'
+)
 
-$dialogPath \
---title $title \
---message $description \
---ontop \
---mini \
---commandfile "$dialogCommandFile" \
---icon $icon \
---button1 \
---button1text $button2 \
---button2 \
---button2text $button1 \
---helpmessage $helpmessage \
-
-if [ $? -eq 2 ]; then
-  # Set the number of iterations
-  iterations=5
+if [[ $exit_status == *5* ]]; then
+    updateScriptLog "Trellix Loop Updates: User selected 5 loops...."
+    iterations=5
+elif [[ $exit_status == *10* ]]; then
+    updateScriptLog "Trellix Loop Updates: User selected 10 loops...."
+    iterations=10
+elif [[ $exit_status == *20* ]]; then
+    updateScriptLog "Trellix Loop Updates: User selected 20 loops...."
+    iterations=20
+elif [[ $exit_status == *50* ]]; then
+    updateScriptLog "Trellix Loop Updates: User selected 50 loops...."
+    iterations=50
 else
-  # Set the number of iterations
-  iterations=20
+    updateScriptLog "Trellix Loop Updates: User selected exit or timed-out"
+    exit 0
 fi
-
 
 # An array containing the list of items to progress through
 itemsToProgress=()
@@ -153,25 +211,21 @@ for element in "${itemsToProgress[@]}"; do
 echo "$element"
 done
 
-# This function sends a command to our command file, and sleeps briefly to avoid race conditions
-function dialog_command()
-{
-echo "$@" >> "$dialogCommandFile"
-sleep 0.1
-}
+sleep 1.0
 
 # Calling our initial dialog window. The & is crucial so that our script progresses.
 # ${#itemsToProgress[@]} is equal to the number of items in our array
+
 $dialogPath \
---title $title \
---message $descriptionloop  \
---ontop \
---mini \
---commandfile "$dialogCommandFile" \
---progress ${#itemsToProgress[@]} \
---icon $icon \
---button1 \
-&
+    --title $title \
+    --message $descriptionloop  \
+    --ontop \
+    --mini \
+    --commandfile "$dialogCommandFile" \
+    --progress ${#itemsToProgress[@]} \
+    --icon $icon \
+    --button1 \ &
+
 
 # Display the bouncy bounce for 2 seconds
 sleep 2
@@ -179,17 +233,17 @@ sleep 2
 # Iterate through our array
 # For each item we've outlined
 for 2 in "${itemsToProgress[@]}"; do
-dialog_command "progress: increment"
-dialog_command "progresstext: $2"
-  echo "Script looping count: $2"
-  /Library/McAfee/agent/bin/cmdagent -c &>/dev/null
-  sleep 5
-  /Library/McAfee/agent/bin/cmdagent -e &>/dev/null
-  sleep 5
-  /Library/McAfee/agent/bin/cmdagent -p &>/dev/null
-  sleep 5
-  /Library/McAfee/agent/bin/cmdagent -f &>/dev/null
-  sleep 5
+    dialog_command "progress: increment"
+    dialog_command "progresstext: $2"
+    updateScriptLog "Trellix Loop Updates: $2"
+    /Library/McAfee/agent/bin/cmdagent -c &>/dev/null
+    sleep 5
+    /Library/McAfee/agent/bin/cmdagent -e &>/dev/null
+    sleep 5
+    /Library/McAfee/agent/bin/cmdagent -p &>/dev/null
+    sleep 5
+    /Library/McAfee/agent/bin/cmdagent -f &>/dev/null
+    sleep 5
 done
 
 # Close our dialog window
@@ -198,5 +252,7 @@ echo "Completed the loop, SwiftDialog closed"
 
 # Delete our command file
 rm "$dialogCommandFile"
+
+updateScriptLog "Trellix Loop Updates: Loop Completed"
 
 exit 0
